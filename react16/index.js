8041e54d501ef711ec6eb88faefc5d97f250d942
waynewang98 <1070546617@qq.com>
2021-09-05 09:57:30 +0800

    Do some preparation to let react16 sub application access micro frontend

--- react16/index.js
+++ react16/index.js
@@ -3,4 +3,24 @@ import "./index.scss"
 import ReactDOM from 'react-dom'
 import BasicMap from './src/router';
 
-ReactDOM.render(<BasicMap />, document.getElementById('app-react'))
+const render = () => {
+  ReactDOM.render(<BasicMap />, document.getElementById('app-react'))
+}
+
+if (!window.__MICRO_WEB__) {
+  render()
+}
+
+// 向外暴露生命周期钩子
+export const bootstrap = () => {
+  console.log('bootstrap')
+}
+
+export const mount = () => {
+  render()
+  console.log('mount')
+}
+
+export const unmount = () => {
+  console.log('unmount', instance)
+}
