05e0b635e9c2edd3f6f747072bd5ba9373eb8da3
waynewang98 <1070546617@qq.com>
2021-09-05 09:18:46 +0800

    Do some preparation to let vue2 sub application access micro frontend

--- service/routes/vue2.js
+++ service/routes/vue2.js
@@ -3,7 +3,6 @@ const router = require('koa-router')()
 router.prefix('/vue2')
 
 router.get('/car/list', function (ctx, next) {
-  // ctx.request.query
   ctx.body = [
     {
       img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1695163309,2577846378&fm=26&gp=0.jpg',
--- vue2/src/main.js
+++ vue2/src/main.js
@@ -4,8 +4,29 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+let instance = null // vue实例
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app-vue')
+const render = () => {
+  instance = new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app-vue')
+}
+
+if (!window.__MICRO_WEB__) { // 不在微前端的环境下
+  render()
+}
+
+// 向外暴露生命周期钩子
+export const bootstrap = () => {
+  console.log('开始加载')
+}
+
+export const mount = () => {
+  render()
+  console.log('渲染成功')
+}
+
+export const unmount = () => {
+  console.log('卸载', instance)
+}
