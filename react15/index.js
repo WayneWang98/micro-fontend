f676ae9a503bc9909ae4a617c3f70f2aa18c3b2d
waynewang98 <1070546617@qq.com>
2021-09-05 09:45:45 +0800

    Do some preparation to let react15 sub application access micro frontend

--- react15/index.js
+++ react15/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom'
 import BasicMap from './src/router/index.jsx';
 import "./index.scss"
 
-ReactDOM.render((
-  <BasicMap />
-), document.getElementById('app-react'))
+const render = () => {
+  ReactDOM.render((
+    <BasicMap />
+  ), document.getElementById('app-react'))
+}
+
+if (!window.__MICRO_WEB__) { // 没有在微前端的环境中
+  render()
+}
+
+// 向外暴露生命周期钩子
+export const bootstrap = () => {
+  console.log('bootstrap')
+}
+
+export const mount = () => {
+  render()
+  console.log('mount')
+}
+
+export const unmount = () => {
+  console.log('unmount', instance)
+}
